075bb005074097d6e7519f18ddd80f5bd0c343b6
alykkehoy <alykkeho@purdue.edu>
2019-02-08 07:01:36 -0500

    parse on program end completed

--- Compiler/Compiler.cpp
+++ Compiler/Compiler.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -14,7 +15,7 @@ string readFile(string file_name) {
 			file_contents += text_line;
 			file_contents.push_back('\n');
 		}
-		cout << file_contents;
+		//cout << file_contents;
 	}
 	else {
 		cout << "Could not open file";
@@ -22,8 +23,25 @@ string readFile(string file_name) {
 	return file_contents;
 }
 
+stack <string> parsePrograms(string program_text) {
+	stack <string> programs;
+
+	int prev_pos = 0;
+	while (program_text.find('$', prev_pos) != string::npos) {
+		int pos = program_text.find('$', prev_pos);
+		string program = program_text.substr(prev_pos, pos - prev_pos);
+		programs.push(program);		
+		prev_pos = pos + 1;
+		
+		cout << program << endl;
+	}
+
+	return programs;
+}
 
 int main()
 {
 	string file_text = readFile("../Compiler/Test.txt");
+	stack <string> programs = parsePrograms(file_text);
+	cout << "Number of programs found: " << programs.size() << endl;
 }
