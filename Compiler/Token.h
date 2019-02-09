c41c08c9baae9b2c2c58e660ee9370800488ebd4
anders <anderslykkehoy@gmail.com>
2019-02-09 08:10:04 -0500

    created tokentype enum

--- Compiler/Token.h
+++ Compiler/Token.h
@@ -1,6 +1,23 @@
 #include <string>
 #pragma once
 
+enum TokenType
+{
+	L_BRACE,
+	R_BRACE,
+	CHAR,
+	ASSIGN_OP,
+	I_TYPE,
+	S_TYPE,
+	B_TYPE,
+	WHILE,
+	IF,
+	PRINT,
+	DIGIT,
+	ADD,
+	EOP
+};
+
 using namespace std;
 
 class Token
