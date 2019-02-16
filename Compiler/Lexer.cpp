0804fac18fef84f32afe336be285f1621d344fc3
anders <anderslykkehoy@gmail.com>
2019-02-16 08:42:48 -0500

    deleted old unused code

--- Compiler/Lexer.cpp
+++ Compiler/Lexer.cpp
@@ -17,7 +17,6 @@ Lexer::~Lexer()
 
 void Lexer::init_map()
 {
-		//STRING_EXP,
 	token_map = {
 		{"}", R_BRACE},
 		{"{", L_BRACE},
@@ -40,7 +39,7 @@ void Lexer::init_map()
 
 	for (int i = 'a'; i <= 'z'; i++) {
 		string s(1, i);
-		token_map.insert(pair<string, TokenType>( s, CHAR));
+		token_map.insert(pair<string, TokenType>(s, CHAR));
 	}
 	for (int i = '0'; i <= '9'; i++) {
 		string s(1, i);
@@ -81,7 +80,6 @@ vector<Token> Lexer::create_tokens(string program_text)
 			//TO HANDLE COMMENTS
 		}
 		else if (is_string_expression(program_text[i])) {
-			//TO HANDLE STRINGS
 			Token token;
 			token.position.first = line_num;
 			token.position.second = i;
@@ -151,13 +149,13 @@ vector<Token> Lexer::validate_tokens(vector<Token> unvalidated_tokens)
 	}
 
 	if (errors > 0) {
-	cout << "INFO Lexer - Lex failed with " << errors << " errors" << endl << endl;
-	cout << "ERROR LIST:" << endl;
-	for (int i = 0; i < error_text.size(); i++) {
-		cout << error_text[i] << endl;
-	}
-	cout << "--------------------------------------" << endl << endl;
-	return unvalidated_tokens;
+		cout << "INFO Lexer - Lex failed with " << errors << " errors" << endl << endl;
+		cout << "ERROR LIST:" << endl;
+		for (int i = 0; i < error_text.size(); i++) {
+			cout << error_text[i] << endl;
+		}
+		cout << "--------------------------------------" << endl << endl;
+		return unvalidated_tokens;
 	}
 
 	//if there were no lexing errors found a notification is given
@@ -236,143 +234,12 @@ void Lexer::verbose_print(vector<Token> tokens)
 
 
 bool Lexer::lex_single(string program_text) {
-	int errors = 0;
-	int line_num = 0;
-	list <Token> program_tokens;
-	vector <string> error_text;
+	remove_comments(program_text);
 
 	vector<Token> unvalidated_tokens = create_tokens(program_text);
 	vector<Token> validated_tokens = validate_tokens(unvalidated_tokens);
 
-	//first we strip comments from the program text string
-	//remove_comments(program_text);
-
-	//then we loop through the text searching for possible tokens till reaching the end of the string
-	//for (int i = 0; i < program_text.length(); i++) {
-	//	if (program_text[i] == '\n') {
-	//		line_num++;
-	//	}
-	//	else if (is_bracket(program_text[i])) {
-	//		program_tokens.push_back(create_bracket_token(program_text[i], line_num, i));
-	//		cout << "DEBUG Lexer - Brace [ " << program_text[i] << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//	}
-	//	else if (is_operator(program_text[i])) {
-	//		program_tokens.push_back(create_operator_token(program_text[i], line_num, i));
-	//		cout << "DEBUG Lexer - Operator [ " << program_text[i] << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//	}
-	//	else if (is_print(program_text, i)) {
-	//		program_tokens.push_back(create_print_token(line_num, i));
-	//		cout << "DEBUG Lexer - Print [ print ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 4;
-	//	}
-	//	else if (is_while(program_text, i)) {
-	//		program_tokens.push_back(create_while_token(line_num, i));
-	//		cout << "DEBUG Lexer - While [ while ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 4;
-	//	}
-	//	else if (is_if(program_text, i)) {
-	//		program_tokens.push_back(create_if_token(line_num, i));
-	//		cout << "DEBUG Lexer - If [ if ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 1;
-	//	}
-	//	else if (is_int(program_text, i)) {
-	//		program_tokens.push_back(create_int_token(line_num, i));
-	//		cout << "DEBUG Lexer - Type [ int ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 2;
-	//	}
-	//	else if (is_string(program_text, i)) {
-	//		program_tokens.push_back(create_string_token(line_num, i));
-	//		cout << "DEBUG Lexer - Type [ string ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 5;
-	//	}
-	//	else if (is_boolean(program_text, i)) {
-	//		program_tokens.push_back(create_boolean_token(line_num, i));
-	//		cout << "DEBUG Lexer - Type [ boolean ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 6;
-	//	}
-	//	else if (is_boolean_value(program_text, i)) {
-	//		program_tokens.push_back(create_boolean_value_token(program_text[i], line_num, i));
-	//		if (program_text[i] == 't') {
-	//			cout << "DEBUG Lexer - BOOLVAL [ true ] found at (" << line_num << ":" << i << ")" << endl;
-	//			i = i + 3;
-	//		}
-	//		else {
-	//			cout << "DEBUG Lexer - BOOLVAL [ false ] found at (" << line_num << ":" << i << ")" << endl;
-	//			i = i + 4;
-	//		}
-	//	}
-	//	else if (is_boolean_operation(program_text, i)) {
-	//		program_tokens.push_back(create_boolean_operation_token(program_text[i], line_num, i));
-	//		cout << "DEBUG Lexer - BOOLOP [ " << program_text.substr(i, 2) << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//		i = i + 1;
-	//	}
-	//	else if (is_assignment(program_text, i)) {
-	//		program_tokens.push_back(create_assignment_token(line_num, i));
-	//		cout << "DEBUG Lexer - ASSIGN_OP [ = ] found at (" << line_num << ":" << i << ")" << endl;
-	//	}
-	//	else if (is_boolean_expression(program_text[i])) {
-	//		program_tokens.push_back(create_boolean_expression_token(program_text[i], line_num, i));
-	//		cout << "DEBUG Lexer - BOOL_EXP [ " << program_text[i] << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//	}
-	//	else if (is_string_expression(program_text[i])) {
-	//		pair<bool, int> result = find_string_end(program_text, i);
-	//		if (!result.first) {
-	//			error_text.push_back("ERROR Lexer - Invalid string expression " + program_text.substr(i, result.second - i));
-	//			cout << error_text.back() << endl;
-	//			errors++;
-	//		}
-	//		else {
-	//			program_tokens.push_back(create_string_expression_token(program_text.substr(i, result.second - i), line_num, i));
-	//			cout << "DEBUG Lexer - STRING_EXP [ " << program_text.substr(i, result.second - i) << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//		}
-	//		i = result.second - 1;
-	//	}
-	//	else if (is_digit(program_text, i)) {
-	//		program_tokens.push_back(create_digit_token(program_text[i], line_num, i));
-	//		cout << "DEBUG Lexer - Digit [ " << program_text[i] << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//	}
-	//	else if (is_char(program_text, i)) {
-	//		program_tokens.push_back(create_char_token(program_text[i], line_num, i));
-	//		cout << "DEBUG Lexer - Char [ " << program_text[i] << " ] found at (" << line_num << ":" << i << ")" << endl;
-	//	}
-	//	//this serves as a catch all for the remaining text that is not a token
-	//	else if (program_text[i] != ' ' && program_text[i] != '\t'){
-	//		int end = i;
-	//		while (end < program_text.length()) {
-	//			if (is_bracket(program_text[end])
-	//				|| is_boolean_expression(program_text[end])
-	//				|| program_text[end] == '\n'
-	//				|| program_text[end] == ' ') {
-	//				error_text.push_back("ERROR Lexer - Error (" + to_string(line_num)
-	//					 + ":" + to_string(i) + ") unrecognized token: " + program_text.substr(i, end - i));
-	//				cout << error_text.back() << endl;
-	//				i = end - 1;
-	//				break;
-	//			}
-	//			end++;
-	//		}
-	//		errors++;
-	//	}
-	//}
-
-
-
-	//if there are any errors the lexer will fail and print out the errors it caught
-	//if (errors > 0) {
-	//	cout << "INFO Lexer - Lex failed with " << errors << " errors" << endl << endl;
-	//	cout << "ERROR LIST:" << endl;
-	//	for (int i = 0; i < error_text.size(); i++) {
-	//		cout << error_text[i] << endl;
-	//	}
-	//	cout << "--------------------------------------" << endl << endl;
-	//	return false;
-	//}
-
-	////if there were no lexing errors found a notification is given
-	////and all of our tokens from this program are added to the vector of all the tokens from all the programs
-	//cout << "INFO Lexer - Lex complete with 0 errors" << endl;
-	//cout << "--------------------------------------" << endl << endl;
-	tokens.push_back(program_tokens);
+	tokens.push_back(validated_tokens);
 	return true;
 }
 
@@ -408,191 +275,6 @@ Token Lexer::create_bracket_token(char character, int line_num, int pos)
 		token.text = "}";
 		token.token_type = R_BRACE;
 	}
-	return token;
-}
-
-bool Lexer::is_operator(char character)
-{
-	return (character == '+');
-}
-
-Token Lexer::create_operator_token(char character, int line_num, int pos)
-{
-	Token token(ADD);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_char(string program_text, int pos)
-{
-	return (isalpha(program_text[pos]) && islower(program_text[pos]) && !isalnum(program_text[pos + 1]));
-}
-
-Token Lexer::create_char_token(char character, int line_num, int pos)
-{
-	Token token(CHAR);
-	token.position.first = line_num;
-	token.position.second = pos;
-	token.character = character;
-	return token;
-}
-
-bool Lexer::is_digit(string program_text, int pos)
-{
-	return (isdigit(program_text[pos]) && !isalnum(program_text[pos + 1]));
-}
-
-Token Lexer::create_digit_token(char character, int line_num, int pos)
-{
-	Token token(DIGIT);
-	token.position.first = line_num;
-	token.position.second = pos;
-	token.digit = (int)character - 48;
-	return token;
-}
-
-bool Lexer::is_print(string program_text, int pos)
-{
-	return (program_text.compare(pos, 5, "print") == 0);
-}
-
-Token Lexer::create_print_token(int line_num, int pos)
-{
-	Token token(PRINT);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_while(string program_text, int pos)
-{
-	return (program_text.compare(pos, 5, "while") == 0);
-}
-
-Token Lexer::create_while_token(int line_num, int pos)
-{
-	Token token(WHILE);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_if(string program_text, int pos)
-{
-	return (program_text.compare(pos, 2, "if") == 0);
-}
-
-Token Lexer::create_if_token(int line_num, int pos)
-{
-	Token token(IF);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_int(string program_text, int pos)
-{
-	return (program_text.compare(pos, 3, "int") == 0);
-}
-
-Token Lexer::create_int_token(int line_num, int pos)
-{
-	Token token(I_TYPE);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_string(string program_text, int pos)
-{
-	return (program_text.compare(pos, 6, "string") == 0);
-}
-
-Token Lexer::create_string_token(int line_num, int pos)
-{
-	Token token(S_TYPE);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_boolean(string program_text, int pos)
-{
-	return (program_text.compare(pos, 7, "boolean") == 0);
-}
-
-Token Lexer::create_boolean_token(int line_num, int pos)
-{
-	Token token(B_TYPE);
-	token.position.first = line_num;
-	token.position.second = pos;
-	return token;
-}
-
-bool Lexer::is_boolean_value(string program_text, int pos)
-{
-	if (program_text.compare(pos, 4, "true") == 0) {
-		return true;
-	}
-	else if (program_text.compare(pos, 5, "false") == 0) {
-		return true;
-	}
-	return false;
-}
-
-Token Lexer::create_boolean_value_token(char character, int line_num, int pos)
-{
-	Token token(BOOL);
-	token.position.first = line_num;
-	token.position.second = pos;
-
-	if (character == 't') {
-		token.boolean = true;
-	}
-	else {
-		token.boolean = false;
-	}
-
-	return token;
-}
-
-bool Lexer::is_boolean_operation(string program_text, int pos)
-{
-	if (program_text.compare(pos, 2, "==") == 0) {
-		return true;
-	}
-	else if (program_text.compare(pos, 2, "!=") == 0) {
-		return true;
-	}
-	return false;
-}
-
-Token Lexer::create_boolean_operation_token(char character, int line_num, int pos)
-{
-	Token token(BOOL_OP);
-	token.position.first = line_num;
-	token.position.second = pos;
-
-	if (character == '=') {
-		token.boolean = true;
-	}
-	else {
-		token.boolean = false;
-	}
-	return token;
-}
-
-bool Lexer::is_assignment(string program_text, int pos)
-{
-	return (program_text[pos] == '=' && program_text[pos + 1] != '=');
-}
-
-Token Lexer::create_assignment_token(int line_num, int pos)
-{
-	Token token(ASSIGN_OP);
-	token.position.first = line_num;
-	token.position.second = pos;
 	return token;
 }
 
@@ -641,13 +323,4 @@ pair<bool, int> Lexer::find_string_end(string program_text, int pos)
 		cout << "WARNING Lexer - No end of string expression found" << endl;
 	}
 	return result;
-}
-
-Token Lexer::create_string_expression_token(string text, int line_num, int pos)
-{
-	Token token(STRING_EXP);
-	token.position.first = line_num;
-	token.position.second = pos;
-	token.text = text;
-	return token;
 }
--- Compiler/Lexer.h
+++ Compiler/Lexer.h
@@ -11,8 +11,8 @@ class Lexer
 {
 public:
 	bool print = false;
-	map<string, TokenType> token_map = { {"a", CHAR} };
-	vector <list <Token>> tokens;
+	map<string, TokenType> token_map;
+	vector <vector <Token>> tokens;
 
 	Lexer();
 	Lexer(bool print);
@@ -33,47 +33,10 @@ public:
 	bool is_bracket(char character);
 	Token create_bracket_token(char character, int line_num, int pos);
 
-	bool is_operator(char character);
-	Token create_operator_token(char character, int line_num, int pos);
-
-	bool is_char(string program_text, int pos);
-	Token create_char_token(char character, int line_num, int pos);
-
-	bool is_digit(string program_text, int pos);
-	Token create_digit_token(char character, int line_num, int pos);
-
-	bool is_print(string program_text, int pos);
-	Token create_print_token(int line_num, int pos);
-
-	bool is_while(string program_text, int pos);
-	Token create_while_token(int line_num, int pos);
-
-	bool is_if(string program_text, int pos);
-	Token create_if_token(int line_num, int pos);
-
-	bool is_int(string program_text, int pos);
-	Token create_int_token(int line_num, int pos);
-
-	bool is_string(string program_text, int pos);
-	Token create_string_token(int line_num, int pos);
-
-	bool is_boolean(string program_text, int pos);
-	Token create_boolean_token(int line_num, int pos);
-
-	bool is_boolean_value(string program_text, int pos);
-	Token create_boolean_value_token(char character, int line_num, int pos);
-
-	bool is_boolean_operation(string program_text, int pos);
-	Token create_boolean_operation_token(char character, int line_num, int pos);
-
-	bool is_assignment(string program_text, int pos);
-	Token create_assignment_token(int line_num, int pos);
-
 	bool is_boolean_expression(char character);
 	Token create_boolean_expression_token(char character, int line_num, int pos);
 
 	bool is_string_expression(char character);
-	Token create_string_expression_token(string text, int line_num, int pos);
 
 	//find string end looks through a possible string expression and returns 2 things in a pair
 	//first a boolean saying if it is a legitamte string expression in the grammer
