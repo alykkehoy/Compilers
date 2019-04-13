5ff5d8400d526842158b880f1f7f7200352d0bc1
anders <anderslykkehoy@gmail.com>
2019-04-13 16:11:49 -0400

    setting up analyze function and storing pointers for analyzer

--- Compiler/SemanticAnalyzer.cpp
+++ Compiler/SemanticAnalyzer.cpp
@@ -13,4 +13,12 @@ SemanticAnalyzer::~SemanticAnalyzer()
 
 void SemanticAnalyzer::analyze(Program& program)
 {
+	current_program = &program;
+	current_tree_node = &program.cst.head;
+	analyze_block();
+}
+
+bool SemanticAnalyzer::analyze_block()
+{
+	return false;
 }
--- Compiler/SemanticAnalyzer.h
+++ Compiler/SemanticAnalyzer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Program.h"
+#include "Tree.h"
 
 class SemanticAnalyzer
 {
@@ -8,5 +9,33 @@ public:
 	~SemanticAnalyzer();
 
 	void analyze(Program& program);
+
+private:
+	Program* current_program = nullptr;
+	table_node* current_table_node;
+	tree_node* current_tree_node;
+	//vector<Token>::iterator current_token;
+
+
+	bool analyze_block();
+	//bool analyze_statement_list();
+	//bool analyze_statement();
+	//bool analyze_print_statement();
+	//bool analyze_assignment_statement();
+	//bool analyze_var_decl();
+	//bool analyze_while_statement();
+	//bool analyze_if_statement();
+	//bool analyze_expr();
+	//bool analyze_int_expr();
+	//bool analyze_string_expr();
+	//bool analyze_boolean_expr();
+	//bool analyze_id();
+	//bool analyze_char_list();
+	//bool analyze_type();
+	//bool analyze_char();
+	//bool analyze_digit();
+	//bool analyze_bool_op();
+	//bool analyze_bool_val();
+	//bool analyze_int_op();
 };
 
