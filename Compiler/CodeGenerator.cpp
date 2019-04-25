51d8e786b80aa1e42fd88850a960fa656d37f1b6
Anders <anders.lykkehoy1@marist.edu>
2019-04-25 15:33:39 -0400

    started setup for generate_code

--- Compiler/CodeGenerator.cpp
+++ Compiler/CodeGenerator.cpp
@@ -7,3 +7,10 @@ CodeGenerator::CodeGenerator(/* args */)
 CodeGenerator::~CodeGenerator()
 {
 }
+
+void CodeGenerator::generate_code(Program& program)
+{
+    this->current_program = &program;
+    this->current_ast = &program.ast;
+    this->current_scope_tree = &program.scope_tree;
+}
--- Compiler/CodeGenerator.h
+++ Compiler/CodeGenerator.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include "Program.h"
+#include "Token.h"
+#include "Tree.h"
+
 class CodeGenerator
 {
 private:
-    /* data */
+    Program* current_program;
+    Tree* current_ast;
+    scope* current_scope_tree;
+
 public:
     CodeGenerator(/* args */);
     ~CodeGenerator();
+
+    void generate_code(Program& program);
 };
