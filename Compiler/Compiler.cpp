7f23874d5966838bd8804159d8d031d4d870acba
Anders <anders.lykkehoy1@marist.edu>
2019-04-27 23:50:30 -0400

    added code gen call to compiler

--- Compiler/Compiler.cpp
+++ Compiler/Compiler.cpp
@@ -8,21 +8,25 @@
 #include "Token.h"
 #include "Lexer.h"
 #include "SemanticAnalyzer.h"
+#include "CodeGenerator.h"
 
 using namespace std;
 
-string readFile(string file_name) {
+string readFile(string file_name)
+{
 	ifstream file(file_name);
 	string text_line;
 	string file_contents;
-	if (file.is_open()) {
+	if (file.is_open())
+	{
 		while (getline(file, text_line))
 		{
 			file_contents += text_line;
 			file_contents.push_back('\n');
 		}
 	}
-	else {
+	else
+	{
 		cout << "Could not open file: " << file_name;
 		exit(1);
 	}
@@ -31,14 +35,17 @@ string readFile(string file_name) {
 
 //takes a string of text from the file and splits it into separate stings for individual programs
 //will also warn the user if there is no '$' found at the end of the file
-vector <Program> parsePrograms(string program_text) {
-	vector <Program> programs;
+vector<Program> parsePrograms(string program_text)
+{
+	vector<Program> programs;
 
 	int program_num = 1;
 	int prev_pos = 0;
-	while (prev_pos < program_text.length() - 1) {
+	while (prev_pos < program_text.length() - 1)
+	{
 		int pos = program_text.find('$', prev_pos);
-		if (pos == string::npos) {
+		if (pos == string::npos)
+		{
 			cout << "WARNING - EOP missing at EOF" << endl;
 			cout << "WARNING - EOP auto added" << endl;
 			program_text.append("$");
@@ -48,7 +55,7 @@ vector <Program> parsePrograms(string program_text) {
 		program_num++;
 
 		programs.push_back(program);
-		prev_pos = pos + 1;		
+		prev_pos = pos + 1;
 	}
 
 	return programs;
@@ -56,50 +63,65 @@ vector <Program> parsePrograms(string program_text) {
 
 //the compiler takes a file name as an argument
 //if no file name if provide it uses the default Test.txt in the Compiler folder
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	string file_text;
-	if (argc == 2) {
+	if (argc == 2)
+	{
 		file_text = readFile(argv[1]);
 	}
-	else {
+	else
+	{
 		file_text = readFile("../Compiler/Test3.txt");
 	}
 
 	//after the file is read in the string is passed to parsePrograms
 	//where it is split at '$' into individual strings representing the programs
-	vector <Program> programs = parsePrograms(file_text);
+	vector<Program> programs = parsePrograms(file_text);
 	cout << "Number of programs found: " << programs.size() << endl;
 
 	//the vector of program strings is passed into the lexer
 	Lexer lexer(true);
 	Parser parser(true);
 	SemanticAnalyzer semantic_analyzer;
+	CodeGenerator code_generator;
 
-	for (int i = 0; i < programs.size(); i++) {
+	for (int i = 0; i < programs.size(); i++)
+	{
 		lexer.lex(programs[i]);
-		if (programs[i].passed_lex) {
+		if (programs[i].passed_lex)
+		{
 			parser.parse(programs[i]);
 		}
 		//cout << programs[i].passed_parse << endl;
-		if (programs[i].passed_parse) {
-			cout << endl << "Program " << i + 1 << " CST:" << endl;
+		if (programs[i].passed_parse)
+		{
+			cout << endl
+				 << "Program " << i + 1 << " CST:" << endl;
 			programs[i].cst.print_tree();
 			//cout << endl;
 		}
-		if (programs[i].passed_parse) {
+		if (programs[i].passed_parse)
+		{
 			semantic_analyzer.analyze(programs[i]);
 			//programs[i].ast.print_tree();
-
 		}
-		if (programs[i].passed_semantic_analysis) {
-			cout << endl << "Program " << i + 1 << " AST:" << endl;
+		if (programs[i].passed_semantic_analysis)
+		{
+			cout << endl
+				 << "Program " << i + 1 << " AST:" << endl;
 			programs[i].ast.print_tree();
 
-			cout << endl << "Program " << i + 1 << " Symbol Table:" << endl;
+			cout << endl
+				 << "Program " << i + 1 << " Symbol Table:" << endl;
 			Tree::print_scope_tree(&programs[i].scope_tree);
 			cout << endl;
 		}
-		cout << "--------------------------------------" << endl << endl;
+		if (programs[i].passed_semantic_analysis)
+		{
+			code_generator.generate_code(programs[i]);
+		}
+		cout << "--------------------------------------" << endl
+			 << endl;
 	}
 }
