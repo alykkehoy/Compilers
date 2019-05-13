2e660f0f406faecb5f976ee1cb79fab65cf50974
anders <anderslykkehoy@gmail.com>
2019-05-13 14:48:11 -0400

    started static table

--- Compiler/CodeGenerator.cpp
+++ Compiler/CodeGenerator.cpp
@@ -10,9 +10,10 @@ CodeGenerator::~CodeGenerator()
 
 void CodeGenerator::generate_code(Program& program)
 {
+	static_id = 0;
     this->current_program = &program;
     this->current_ast = &program.ast.head;
-    this->current_scope_tree = &program.scope_tree;
+    this->current_scope = &program.scope_tree;
 
 	current_ast = current_ast->children[0].get();
 	gen_block();
@@ -25,6 +26,10 @@ void CodeGenerator::generate_code(Program& program)
 //TODO
 bool CodeGenerator::gen_block()
 {
+	//if (current_scope->parent != nullptr) {
+	//	current_scope = current_scope->children
+	//}
+
 	for (int i = 0; i < current_ast->children.size(); i++) {
 		switch (current_ast->children[i]->node_type)
 		{
@@ -34,6 +39,9 @@ bool CodeGenerator::gen_block()
 		case WHILE_STATEMENT:
 			break;
 		case PRINT:
+			current_ast = current_ast->children[i].get();
+			gen_print();
+			current_ast = current_ast->parent;
 			break;
 		case ASSIGNMENT_STATEMENT:
 			current_ast = current_ast->children[i].get();
@@ -50,6 +58,58 @@ bool CodeGenerator::gen_block()
 		}
 	}
 
+	return false;
+}
+
+//TODO print(1)
+bool CodeGenerator::gen_print()
+{
+	if (current_ast->children.size() == 1) {
+		auto scope_row = Tree::find_var(current_scope, current_ast->children[0]->token->text[0]);
+
+		switch (scope_row->type)
+		{
+		case I_TYPE:
+			gen_print_int();
+			break;
+		case S_TYPE:
+			gen_print_string();
+			break;
+		case B_TYPE:
+			gen_print_bool();
+			break;
+		default:
+			break;
+		}
+	}
+
+	return false;
+}
+
+//TODO
+bool CodeGenerator::gen_print_int()
+{
+	current_program->code += "AC";
+	//var loc
+	current_program->code += "XX";
+	current_program->code += "XX";
+
+	current_program->code += "A2";
+	current_program->code += "01";
+	current_program->code += "FF";
+
+	return false;
+}
+
+//TODO
+bool CodeGenerator::gen_print_string()
+{
+	return false;
+}
+
+//TODO
+bool CodeGenerator::gen_print_bool()
+{
 	return false;
 }
 
@@ -76,13 +136,20 @@ bool CodeGenerator::gen_var_decl()
 //TODO
 bool CodeGenerator::gen_int()
 {
+	std::shared_ptr<static_row> new_row(new static_row);
+	new_row->var = current_ast->children[1]->token->text[0];
+	new_row->temp_loc = "T" + std::to_string(static_id) + "XX";
+	static_id++;
+
 	current_program->code += "A9";
 	current_program->code += "00";
 	current_program->code += "8D";
 	//temp location for var
-	current_program->code += "XX";
-	current_program->code += "XX";
+	current_program->code += new_row->temp_loc;
+	//current_program->code += "XX";
+	//current_program->code += "XX";
 
+	static_table.push_back(new_row);
 	return false;
 }
 
@@ -101,12 +168,28 @@ bool CodeGenerator::gen_boolean()
 //TODO look up type and go to that code gen function
 bool CodeGenerator::gen_assignment()
 {
-	gen_assign_int();
+	auto scope_row = Tree::find_var(current_scope, current_ast->children[0]->token->text[0]);
+
+	switch (scope_row->type)
+	{
+	case I_TYPE:
+		gen_assign_int();
+		break;
+	case S_TYPE:
+		gen_assign_string();
+		break;
+	case B_TYPE:
+		gen_assign_bool();
+		break;
+	default:
+		break;
+	}
+
 	return false;
 }
 
 //TODO var loc
-//TODO a + 1, 1 + 1, etc
+//TODO a = b, a + 1, 1 + 1, etc
 bool CodeGenerator::gen_assign_int()
 {
 	if (current_ast->children.size() == 2) {
@@ -130,4 +213,9 @@ bool CodeGenerator::gen_assign_string()
 bool CodeGenerator::gen_assign_bool()
 {
 	return false;
+}
+
+std::shared_ptr<static_row> CodeGenerator::find_static_row(char var)
+{
+	return std::shared_ptr<static_row>();
 }
--- Compiler/CodeGenerator.h
+++ Compiler/CodeGenerator.h
@@ -4,12 +4,21 @@
 #include "Token.h"
 #include "Tree.h"
 
+struct static_row
+{
+	string temp_loc = "XXXX";
+	char var;
+	string loc;
+};
+
 class CodeGenerator
 {
 private:
     Program* current_program;
     tree_node* current_ast;
-    scope* current_scope_tree;
+    scope* current_scope;
+	int static_id;
+	std::vector<std::shared_ptr<static_row>> static_table;
 
 public:
     CodeGenerator(/* args */);
@@ -18,10 +27,10 @@ public:
     void generate_code(Program& program);
 
 	bool gen_block();
-	//bool parse_statement_list();
-	//bool parse_statement();
-	//bool parse_print_statement();
-	//bool parse_assignment_statement();
+	bool gen_print();
+	bool gen_print_int();
+	bool gen_print_string();
+	bool gen_print_bool();
 	bool gen_var_decl();
 	bool gen_int();
 	bool gen_string();
@@ -30,19 +39,6 @@ public:
 	bool gen_assign_int();
 	bool gen_assign_string();
 	bool gen_assign_bool();
-	//bool parse_while_statement();
-	//bool parse_if_statement();
-	//bool parse_expr();
-	//bool parse_int_expr();
-	//bool parse_string_expr();
-	//bool parse_boolean_expr();
-	//bool parse_id();
-	//bool parse_char_list();
-	//bool parse_type();
-	//bool parse_char();
-	//bool parse_digit();
-	//bool parse_bool_op();
-	//bool parse_bool_val();
-	//bool parse_int_op();
 
+	std::shared_ptr<static_row> find_static_row(char var);
 };
