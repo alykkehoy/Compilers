fa829645f36f8f98c26c28e0f46fd36ff3747ec6
anders <anderslykkehoy@gmail.com>
2019-05-14 07:06:21 -0400

    more bugs

--- Compiler/CodeGenerator.cpp
+++ Compiler/CodeGenerator.cpp
@@ -70,7 +70,7 @@ bool CodeGenerator::gen_block()
 	return false;
 }
 
-//TODO
+//TODO != and jumps not correct
 bool CodeGenerator::gen_if()
 {
 	//if(a == _)
@@ -173,18 +173,126 @@ bool CodeGenerator::gen_if()
 //TODO
 bool CodeGenerator::gen_while()
 {
-	//TODO copy gen if
+	int while_start = current_program->code.length() - 1;
+
+	//if(a == _)
+	if (current_ast->children[2]->node_type != CHAR) {
+		current_program->code += "A2";
+
+		if (current_ast->children[2]->node_type == DIGIT)
+		{
+			current_program->code += "0" + current_ast->children[2]->token->text;
+		}
+		else
+		{
+			if (current_ast->children[2]->token->text[0] == 't')
+			{
+				current_program->code += "00";
+			}
+			else
+			{
+				current_program->code += "01";
+			}
+		}
+
+		current_program->code += "EC";
+		current_program->code += find_static_row(current_ast->children[0]->token->text[0])->temp_loc;
+	}
+	else
+	{
+		//if(a == b)
+		if (current_ast->children[0]->node_type == CHAR) {
+			current_program->code += "AE";
+			current_program->code += find_static_row(current_ast->children[0]->token->text[0])->temp_loc;
+			current_program->code += "EC";
+			current_program->code += find_static_row(current_ast->children[2]->token->text[0])->temp_loc;
+
+		}
+		//if(1 == a)
+		else if (current_ast->children[0]->node_type == DIGIT)
+		{
+			current_program->code += "A2";
+			current_program->code += "0" + current_ast->children[0]->token->text;
+			current_program->code += "EC";
+			current_program->code += find_static_row(current_ast->children[2]->token->text[0])->temp_loc;
+		}
+		//if(true == a)
+		else
+		{
+			current_program->code += "A2";
+
+			if (current_ast->children[0]->token->text[0] == 't')
+			{
+				current_program->code += "00";
+			}
+			else
+			{
+				current_program->code += "01";
+			}
+			current_program->code += "EC";
+			current_program->code += find_static_row(current_ast->children[2]->token->text[0])->temp_loc;
+		}
+	}
+
+
+	//!= vs ==
+	bool t_or_f;
+	int jump_start;
+	if (current_ast->children[1]->token->text[0] == '=') {
+		t_or_f = true;
+		current_program->code += "D0";
+
+		jump_start = current_program->code.length() - 1;
+
+		current_program->code += "JJ";
+	}
+	else {
+		t_or_f = false;
+	}
+
 
 	//gen block
 	current_ast = current_ast->children[3].get();
 	gen_block();
 	current_ast = current_ast->parent;
 
-	//TODO need false statement to jump back to start
 
+	//TODO need false statement to jump back to start
+	current_program->code += "A2";
+	current_program->code += "01";
+	current_program->code += "EC";
+
+	current_program->code += "5E";
+	current_program->code += "00";
+
+	current_program->code += "D0";
+	//go around
+	int difference = (current_program->code.length() - 1) - while_start;
+	difference = 191 - difference;
+
+	std::stringstream stream_difference;
+	stream_difference << std::setfill('0') << std::setw(2) << std::hex << difference;
+
+	current_program->code += stream_difference.str();
+
+
+	//jump
+	if (t_or_f == true) {
+		int distance = ((current_program->code.length() - 1) - jump_start) / 2 - 1;
+
+		std::stringstream stream;
+		stream << std::setfill('0') << std::setw(2) << std::hex << distance;
+
+		current_program->code[jump_start + 1] = stream.str()[0];
+		current_program->code[jump_start + 2] = stream.str()[1];
+	}
+	else {
+
+	}
 	return false;
 }
 
+//TODO print("test")
 bool CodeGenerator::gen_print()
 {
 	//print(a)
@@ -210,7 +318,26 @@ bool CodeGenerator::gen_print()
 
 		//print("test")
 		if (current_ast->children[0]->node_type == STRING_EXP) {
+			std::stringstream stream;
+			for (int i = 1; i < current_ast->children[0]->token->text.length() - 1; i++) {
+				stream << std::hex << (int)current_ast->children[0]->token->text[i];
+			}
+			stream << "00";
+			heap.insert(0, stream.str());
 
+			int loc = (192 - (heap.length() - 1)) / 2;
+
+			std::stringstream stream_loc;
+			stream_loc << std::setfill('0') << std::setw(2) << std::hex << loc;
+			stream_loc << "00";
+
+			current_program->code += "AC";
+
+			current_program->code += stream_loc.str();
+
+			current_program->code += "A2";
+			current_program->code += "02";
+			current_program->code += "FF";
 		}
 		//print(1)
 		else if(current_ast->children[0]->node_type == DIGIT) {
@@ -259,7 +386,6 @@ bool CodeGenerator::gen_print_int()
 	return false;
 }
 
-//TODO test
 bool CodeGenerator::gen_print_string()
 {
 	current_program->code += "AC";
@@ -325,7 +451,6 @@ bool CodeGenerator::gen_int()
 	return false;
 }
 
-//TODO test
 bool CodeGenerator::gen_string()
 {
 	std::shared_ptr<static_row> new_row(new static_row);
@@ -418,7 +543,6 @@ bool CodeGenerator::gen_assign_int()
 	return false;
 }
 
-//TODO
 bool CodeGenerator::gen_assign_string()
 {
 	std::stringstream stream;
@@ -427,14 +551,12 @@ bool CodeGenerator::gen_assign_string()
 	}
 	stream << "00";
 	heap.insert(0, stream.str());
-	//cout << stream.str() << endl;
 
 	current_program->code += "A9";
 	int loc = (192 - (heap.length() - 1)) / 2;
 
 	std::stringstream stream_loc;
 	stream_loc << std::setfill('0') << std::setw(2) << std::hex << loc;
-	//cout << stream_loc.str() << endl;
 	current_program->code += stream_loc.str();
 	current_program->code += "8D";
 
